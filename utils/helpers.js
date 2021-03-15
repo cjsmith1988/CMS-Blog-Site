0cfd080de1cb93af90f840f391b1566fc1426f20
Curtis Smith <cj1988333@hotmail.com>
2021-03-15 09:04:35 -0400

    fixed day array in date function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,10 @@
 module.exports = {
     format_date: date => {
-        const months = ["Jan", "Feb", "Mar", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         const days = ["1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th","11th","12th","13th","14th","15th","16th","17th","18th","19th","20th",
                         "21st","22nd","23rd","24th","25th","26th","27th","28th","29th","30th","31st"];
 
-      return `${months[new Date(date).getMonth()]} ${days[new Date(date).getDate()]}, ${new Date(
+      return `${months[new Date(date).getMonth()]} ${days[new Date(date).getDate()-1]}, ${new Date(
         date
       ).getFullYear()}`;
     },
